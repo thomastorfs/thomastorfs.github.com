09344574925ab5c040861b2e96cb14becdb95746
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-15 21:52:23 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 
-# Many thanks to edrex for this script
-# https://gist.github.com/edrex/7492725
-
-
-# replace with your compile command
-#roots clean
+roots clean
 roots compile --env="production"
 
-# Remove the DS_Store files
-find ./public -type f -name .DS_Store -exec rm {} +
-rm ./public/README
-
-# removes the html extension
-for f in `ls public/*.html public/**/*.html`; do mv $f "${f%%.*}"; done
-
-# requires s3cmd >= v1.5.0-beta1 for
-# https://github.com/s3tools/s3cmd/issues/243
-s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed public/ s3://thomastorfs.com/
-
-# Set cache control to one hour
-s3cmd --recursive modify --add-header='Cache-Control:max-age=3600, public' s3://thomastorfs.com
+git add .
+git commit -m "publish new version to github pages"
+git push
+git subtree push --prefix public origin gh-pages
