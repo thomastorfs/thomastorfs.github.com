523381410772eda67afd7a0a88729ba6f1e2ff89
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-15 22:10:26 +0200

    Edit .gitignore to publish

--- publish.sh
+++ publish.sh
@@ -3,7 +3,14 @@
 roots clean
 roots compile --env="production"
 
+sed -i "" '/gh-pages/d' ./.gitignore
+git add .
+git commit -m "Edit .gitignore to publish"
+
 git add .
 git commit -m "publish new version to github pages"
 git push
 git subtree push --prefix public origin gh-pages
+
+git reset HEAD~
+git checkout .gitignore
