317bc7958ca60ac04222d0e7c36d9b3bfa6f0d5b
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-16 21:31:33 +0200

    Update git commit message

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ roots compile --env="production"
 
 sed -i "" '/public\//d' ./.gitignore
 git add .
-git commit -m "Edit .gitignore to publish"
+git commit -m "Publish new static version"
 
 git push origin `git subtree split --prefix public`:gh-pages --force
 
