600a7d4808adfe58e5333eaa9a0524ce6f879293
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-17 22:47:43 +0200

    also publish to gitlab

--- publish.sh
+++ publish.sh
@@ -8,6 +8,7 @@ git add .
 git commit -m "Publish new static version"
 
 git push origin `git subtree split --prefix public`:gh-pages --force
+git push gitlab-public `git subtree split --prefix public`:master --force
 
 git reset HEAD~
 git checkout .gitignore
