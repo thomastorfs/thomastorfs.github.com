c22a89eb44a6af3cc7fe346b70d92993d77a93a8
Thomas Torfs <thomas.torfs@gmail.com>
2021-09-21 21:07:55 +0200

    update publish script

--- publish.sh
+++ publish.sh
@@ -3,12 +3,8 @@
 roots clean
 roots compile --env="production"
 
-sed -i "" '/public\//d' ./.gitignore
 git add .
 git commit -m "Publish new static version"
 
-git push origin `git subtree split --prefix public`:gh-pages --force
-git push gitlab-public `git subtree split --prefix public`:master --force
-
-git reset HEAD~
-git checkout .gitignore
+git push -u origin --all
+git push -u gitlab-public --all
