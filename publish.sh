fa330d88f760c17107ba480ebabf0280c9c071df
Thomas Torfs <thomas.torfs@gmail.com>
2022-03-31 08:40:04 +0200

    feat: add bitbucket

--- publish.sh
+++ publish.sh
@@ -10,4 +10,5 @@ git add .
 git commit -m "Publish new static version"
 
 git push -u gitlab --all
+git push -u bitbucket --all
 git push -u origin --all
--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 git push -u gitlab --all
+git push -u bitbucket --all
 git push -u origin --all
